14e22f47c2ae3abd8bcaa2f179955781e7c4dbeb
Diyorbek <diyorbekabdumannonov4@gmail.com>
2023-05-23 16:55:57 +0500

    changes

--- config/plugins.js
+++ config/plugins.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   "vercel-deploy": {
     enabled: true,
     config: {
-      deployHook: "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
+      deployHook: "https://api.vercel.com/v1/integrations/deploy/prj_https://api.vercel.com/v1/integrations/deploy/prj_9tv2UOESM9C1H4wa557UaMJx6BYT/HSciLpL3ug",
       apiToken: "p43PINnME1r18fTgPc9Qc8U1",
       appFilter: "strapi",
       roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
